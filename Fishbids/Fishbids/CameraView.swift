53e60a71c8b8d80239815c1f3340cf0f3651c094
Neo <neo@orz.hk>
2016-04-24 03:48:06 +0800

    add different overlays

--- Fishbids/Fishbids/CameraView.swift
+++ Fishbids/Fishbids/CameraView.swift
@@ -9,13 +9,22 @@
 import UIKit
 
 class CameraView: UIView {
-
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+    @IBAction func valueChanged(sender: AnyObject) {
+        print(pageController.currentPage)
+        
+        if (pageController.currentPage == 1) {
+            imageOverlay.image = UIImage.init(imageLiteral: "reference1")
+        } else if (pageController.currentPage == 2) {
+            imageOverlay.image = UIImage.init(imageLiteral: "reference2")
+        } else if (pageController.currentPage == 3) {
+            imageOverlay.image = UIImage.init(imageLiteral: "reference3")
+        } else {
+            imageOverlay.image = UIImage.init(imageLiteral: "reference0")
+        }
     }
-    */
+    
+    @IBOutlet weak var imageOverlay: UIImageView!
+    
+    @IBOutlet weak var pageController: UIPageControl!
 
 }
